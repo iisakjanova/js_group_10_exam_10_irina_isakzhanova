c24d819b1719c687954e22c4fd377a96e2444f56
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-10-23 19:57:42 +0600

    Added comments to database script

--- database.sql
+++ database.sql
@@ -32,3 +32,8 @@ INSERT INTO posts (title, content, datetime, image)
 VALUES ('Post 1', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit vel vero. Accusamus, beatae consequuntur cumque ', '2021-10-19 20:14:10', 'image1.jpg'),
        ('Post 2', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit vel vero. Accusamus, beatae consequuntur cumque ', '2021-10-19 20:15:10', 'image2.jpg'),
        ('Post 3', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit vel vero. Accusamus, beatae consequuntur cumque ', '2021-10-19 20:16:10', 'image3.jpg');
+
+INSERT INTO comments (author, content, post_id)
+VALUES ('somebody', 'Lorem ipsum dolor sit amet', 1),
+       ('somebody2', 'Lorem ipsum dolor sit amet', 1),
+       ('Anonymous', 'Lorem ipsum dolor sit amet', 1);
